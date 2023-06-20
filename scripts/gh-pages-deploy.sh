d5a59a8bddbcb0d43d758fda694b05abfe20e3e0
Joel <j.settrbrg@gmail.com>
2023-06-20 12:07:55 +0200

    Change from master to main

--- scripts/gh-pages-deploy.sh
+++ scripts/gh-pages-deploy.sh
@@ -9,6 +9,6 @@ git --work-tree $dist_dir commit -m $dist_branch
 echo "Pushing to gh-pages..."
 git push origin HEAD:$dist_branch --force
 rm -r $dist_dir
-git checkout -f master
+git checkout -f main
 git branch -D $dist_branch
 echo "Successfully deployed..."
